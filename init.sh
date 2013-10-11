aaa7adaf5f79139adea89c7855a156b8b6679cc6
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-10-11 15:00:18 +0530

    Updated

--- init.sh
+++ init.sh
@@ -3,7 +3,15 @@ sudo apt-get -y install vim
 
 # You can try any of the fluxbox, blackbox or openbox
 sudo apt-get -y install xorg fluxbox #MINIMAL UBUNTU
+sudo apt-get -y install openjdk-6-jdk
 
+if [ -f "/home/vagrant/android-studio-bundle-130.737825-linux.tgz" ];
+then
+ echo "Android Studio exists."
+else
+ echo "Downloading Android Studio ..."
+ cd /home/vagrant/ && wget http://dl.google.com/android/studio/android-studio-bundle-130.737825-linux.tgz
+fi
 
 #MEMORY ALLOCATED TO A VAGARNT BOX IS 350MB
 #THIS MAY BE SLOW
